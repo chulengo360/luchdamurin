4440fda95230ee3917b714a370fd34a45803ffcd
chulengo360 <riccardo.tiddi@gmail.com>
2024-04-11 07:53:39 -0400

    Commit

---
+++ data.js
@@ -0,0 +1,498 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-esterno",
+      "name": "Esterno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.5959988826956106,
+        "pitch": -0.20267530615227614,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7592324968161712,
+          "pitch": -0.1796904484739663,
+          "rotation": 0,
+          "target": "6-ingresso"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.448502216929688,
+          "pitch": -0.4380710043291671,
+          "title": "L'agriturismo per le vostre vacanze in Alto Adige",
+          "text": "<div>Il Lüch da Murin è un maso ricco di storia e di storie da raccontare. Murin è il nome ladino per mulino. Qui infatti vi era un mulino con ben 4 ruote spinte dall'acqua della Gran Ega (Rio Gadera). È stato in funzione sino ai primi anni del novecento. Dagli anni '50 l'acqua è utilizzata per produrre energia elettrica pulita. Del vecchio mulino abbiamo conservato alcune macine e qualche oggetto da farvi vedere.</div><div><br></div><div>La sede del maso, cioè la stalla con fienile e la casa di famiglia, è salda al suo posto da centinaia di anni. È dal 1296 che questo luogo in riva al fiume è abitato da agricoltori, mugnai e artigiani. La nostra famiglia vive e lavora qui sin dalla metà del settecento. L'attività principale è da sempre l'allevamento di bestiame. Lo sfalcio, ovvero la raccolta del fieno sui verdi prati nei dintorni, è uno dei lavori più importanti. Il profumato fieno è l'alimento principale per i nostri animali durante la stagione fredda. Tuttora il maso è un'azienda agricola a gestione familiare dove tradizione e innovazione s'incontrano in una piccola attività di nicchia. Con l'allevamento di razze di animali rare, vogliamo contribuire a mantenere la biodiversità. L'attività agrituristica con un appartamento per le Vostre vacanze e la \"fattoria didattica\" per scolaresche e gruppi, integrano l'economia del maso.</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-camera-da-letto",
+      "name": "Camera da letto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.18699956271367313,
+        "pitch": 0.19054105343679417,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0076920574070503,
+          "pitch": 0.24336013348122165,
+          "rotation": 0,
+          "target": "4-corridoio2"
+        },
+        {
+          "yaw": -0.7430401275323373,
+          "pitch": 0.18305219655542437,
+          "rotation": 0,
+          "target": "8-stube2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cameretta",
+      "name": "Cameretta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.4222530432613656,
+        "pitch": 0.37641851465659215,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3490434917498693,
+          "pitch": 0.0891994128522029,
+          "rotation": 0,
+          "target": "3-corridoio1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-corridoio1",
+      "name": "Corridoio1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.3742605315129666,
+        "pitch": 0.10116077765463416,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12553995205166046,
+          "pitch": 0.249900924472076,
+          "rotation": 0,
+          "target": "4-corridoio2"
+        },
+        {
+          "yaw": 1.8648847651606104,
+          "pitch": 0.10409981195153684,
+          "rotation": 0,
+          "target": "2-cameretta"
+        },
+        {
+          "yaw": 2.9571796294369923,
+          "pitch": 0.15169264796494275,
+          "rotation": 0,
+          "target": "9-bagno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-corridoio2",
+      "name": "Corridoio2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.2335052802854776,
+        "pitch": 0.24666630326831296,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.444251756777465,
+          "pitch": 0.14332275053223142,
+          "rotation": 0,
+          "target": "6-ingresso"
+        },
+        {
+          "yaw": -1.038078093595077,
+          "pitch": 0.29533076033556505,
+          "rotation": 0,
+          "target": "7-stube1"
+        },
+        {
+          "yaw": -1.9004251829454724,
+          "pitch": 0.3191684226583913,
+          "rotation": 0,
+          "target": "5-cucina"
+        },
+        {
+          "yaw": 1.444111642884824,
+          "pitch": 0.31241967182477737,
+          "rotation": 0,
+          "target": "1-camera-da-letto"
+        },
+        {
+          "yaw": 1.7135025891242144,
+          "pitch": 0.19656589314823947,
+          "rotation": 0,
+          "target": "3-corridoio1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-cucina",
+      "name": "Cucina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.04986655005698104,
+        "pitch": 0.13924153904995684,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1092983943012573,
+          "pitch": 0.10102014047517471,
+          "rotation": 0,
+          "target": "4-corridoio2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ingresso",
+      "name": "Ingresso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.8445018593259128,
+        "pitch": 0.14491047298423965,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.45540828231906616,
+          "pitch": 0.13041017024181656,
+          "rotation": 0,
+          "target": "0-esterno"
+        },
+        {
+          "yaw": 0.8193472024187969,
+          "pitch": 0.26196925368926927,
+          "rotation": 0,
+          "target": "4-corridoio2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-stube1",
+      "name": "Stube1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.0311991520612125,
+        "pitch": 0.3448645320319059,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.127934893822907,
+          "pitch": 0.3399238704910612,
+          "rotation": 0,
+          "target": "4-corridoio2"
+        },
+        {
+          "yaw": 2.148209250824639,
+          "pitch": 0.27855627116219495,
+          "rotation": 0,
+          "target": "8-stube2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-stube2",
+      "name": "Stube2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.4168099630347397,
+        "pitch": 0.44176625185900775,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2599079754791127,
+          "pitch": 0.3028622771768834,
+          "rotation": 0,
+          "target": "7-stube1"
+        },
+        {
+          "yaw": 2.691105244573265,
+          "pitch": 0.5265992206774435,
+          "rotation": 0,
+          "target": "1-camera-da-letto"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-bagno",
+      "name": "Bagno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.1959963384905521,
+        "pitch": 0.5353560431306583,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5244090955616834,
+          "pitch": 0.22634209679480932,
+          "rotation": 0,
+          "target": "3-corridoio1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lüch da Murin - Val Badia",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
