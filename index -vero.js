e3af4a283f8f593ca9320e2efb0a15ff54a1370a
chulengo360 <riccardo.tiddi@gmail.com>
2024-04-13 10:38:08 +0200

    Commit

--- index -vero.js
+++ index -vero.js
@@ -110,6 +110,8 @@
   // Set up autorotate, if enabled.
   var autorotate = Marzipano.autorotate({
     yawSpeed: 0.03,
+    pitchSpeed: 1,
+    fovSpeed: 1,
     targetPitch: 0,
     targetFov: Math.PI/2
   });
@@ -184,32 +186,25 @@
     return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
   }
 
-
   function switchScene(scene) {
     stopAutorotate();
     scene.view.setParameters(scene.data.initialViewParameters);
     scene.scene.switchTo();
      // Start with the Autorate on desktop.
     if (!document.body.classList.contains('mobile')) {
-        startAutorotate();
+       startAutorotate();
     }
     else  {
         hideSceneList();
-	enableGiro(scene); // Questo prima stava fuori sotto a update SceneList(scene);
+enableGiro(scene); //forse questo va messo in else assieme a hide
     }
     updateSceneName(scene);
     updateSceneList(scene);
     
-    //tento di risolvere il problema del toggleAutorotate se una info e' aperta quando si cambia scena
-    /*if (!autorotateToggleElement.classList.contains('enabled')) {
-        if (!document.body.classList.contains('mobile')) {
-            toggleAutorotate();
-        }
-    }*/
   }
 
   function updateSceneName(scene) {
-    sceneNameElement.innerHTML = "Lüch da Murin: " + sanitize(scene.data.name);
+    sceneNameElement.innerHTML = sanitize(scene.data.name);
   }
 
   function updateSceneList(scene) {
@@ -312,11 +307,6 @@
     return wrapper;
   }
 
-
-    //Sempre provando a risolvere il problema di molteplici infohotspots aperti e autorate
-    var InfoHotspotsAperti = 0; 
-    
-    
   function createInfoHotspotElement(hotspot) {
 
     // Create wrapper element to hold icon and tooltip.
@@ -351,8 +341,6 @@
     closeIcon.src = 'img/close.png';
     closeIcon.classList.add('info-hotspot-close-icon');
     closeWrapper.appendChild(closeIcon);
-    
-
 
     // Construct header element.
     header.appendChild(iconWrapper);
@@ -374,54 +362,14 @@
     modal.classList.add('info-hotspot-modal');
     document.body.appendChild(modal);
 
-
-
- 
-    var aperto = 0;
     var toggle = function() {
       wrapper.classList.toggle('visible');
       modal.classList.toggle('visible');
-      //PROBLEMA SE UN INFO HOTSPOT e' GIA APERTO, APRIRNE UN ALTRO RIATTIVA LA AUTOROTATE
-      if(aperto==0){
-          //if(InfoHotspotsAperti <1){
-          //autorotateToggleElement.classList.remove('enabled');
-          //stopAutorotate();
-          /*removeElementsByClass('info-hotspot');
-          data.infoHotspots.forEach(function(hotspot) {
-              var element = createInfoHotspotElement(hotspot);
-              scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
-          });*/
-          InfoHotspotsAperti++;
-          aperto = 1;
-      }
-      /*else{
-          //if (autorotateToggleElement.classList.contains('enabled')) {
-            autorotateToggleElement.classList.add('enabled');          
-            startAutorotate();
-            if (InfoHotspotsAperti>1){ document.write(InfoHotspotsAperti);}
-            InfoHotspotsAperti--;
-            
-          //}
-      }
-      }*/
-      else{
-          aperto=0;
-          InfoHotspotsAperti--;
-      }
-      if(InfoHotspotsAperti <1){
-          autorotateToggleElement.classList.add('enabled');          
-          startAutorotate();
-      }  
-      else{
-          autorotateToggleElement.classList.remove('enabled');
-          stopAutorotate();
-      }
-       //if (InfoHotspotsAperti>2){ document.write(InfoHotspotsAperti);}
     };
 
     // Show content when hotspot is clicked.
     wrapper.querySelector('.info-hotspot-header').addEventListener('click', toggle);
-    
+
     // Hide content when close icon is clicked.
     modal.querySelector('.info-hotspot-close-wrapper').addEventListener('click', toggle);
 
@@ -431,16 +379,6 @@
 
     return wrapper;
   }
-
-    //FUNZIONE CHE CHIUDE TUTTI GLI INFO HOTSPOT APERTI
-
-/*function removeElementsByClass(className){
-var elements = document.getElementsByClassName(className);
-while(elements.length > 0){
- if(elements[0].parentNode.removeChild(elements[0]);
-}
-}*/
-
 
   // Prevent touch and scroll events from reaching the parent element.
   function stopTouchAndScrollEventPropagation(element, eventList) {
@@ -473,5 +411,6 @@ while(elements.length > 0){
 
   // Display the initial scene.
   switchScene(scenes[0]);
+  			
 
 })();
